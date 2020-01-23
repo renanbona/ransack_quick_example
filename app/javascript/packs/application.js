121f8d0a1edc5907ab768443471616fcc78b1017
Renan Bona <rcbp3094@gmail.com>
2020-01-23 02:06:16 -0300

    Create Home page and add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require vendor/jqBootstrapValidation.js
+//= require vendor/contact_me.js
+//= require vendor/freelancer.min.js
+//= require vendor/jquery-easing/jquery.easing.min.js
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
